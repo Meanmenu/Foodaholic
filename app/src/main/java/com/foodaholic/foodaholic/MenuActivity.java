8876a0ff6489de45fc16a43b7544d11349201252
Carlos <palacin38@gmail.com>
2015-11-24 23:30:09 -0800

    Added EddystoneScannerService to dtect nearby beacons

--- app/src/main/java/com/foodaholic/foodaholic/MenuActivity.java
+++ app/src/main/java/com/foodaholic/foodaholic/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.foodaholic.foodaholic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.foodaholic.foodaholic.service.EddystoneScannerService;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Intent serviceIntent = new Intent(this, EddystoneScannerService.class);
+        startService(serviceIntent);
     }
 }
